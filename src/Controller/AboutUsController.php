96c3070dc520ed83bb448dcf4ea6af8650c8ac6e
Mathias <mlj.bouillon@outlook.fr>
2020-01-30 16:21:54 +0100

    CRUD Crew

--- src/Controller/AboutUsController.php
+++ src/Controller/AboutUsController.php
@@ -4,6 +4,7 @@
 namespace App\Controller;
 
 
+use App\Repository\CrewRepository;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
@@ -13,8 +14,10 @@ class AboutUsController extends AbstractController
     /**
      * @Route("/about_us", name="about_us")
      */
-    public function index(): Response
+    public function index(CrewRepository $crewRepository): Response
     {
-        return $this->render('about_us/index.html.twig');
+        return $this->render('about_us/index.html.twig', [
+            'crews' => $crewRepository->findAll(),
+        ]);
     }
 }
